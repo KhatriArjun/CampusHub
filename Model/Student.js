a69f35448e56c9d94383dd9d9c39f0ee5b776e5d
ghanashyamkh <khanal.ghanashyam116@gmail.com>
2023-12-05 19:34:35 +0545

    added student,teacher json file

---
+++ Model/Student.js
@@ -0,0 +1,21 @@
+
+
+
+import mongoose from 'mongoose';
+
+const Student = new mongoose.Schema({
+
+  id:{type : String,require : true},
+  username: {type : String , require : true},
+  password:{type : String , require : true},
+  email:{type : String , require : true},
+  s_name : {type : String},
+  batch : {type : String , require : true},
+  subjects : {type : [String] , require : true}
+  
+
+});
+
+const Student_Modal = mongoose.model('Student', Student);
+
+export default Student_Modal
---
+++ Model/Teaher.js
@@ -0,0 +1,20 @@
+
+
+
+import mongoose from 'mongoose';
+
+const Teacher = new mongoose.Schema({
+
+  id:{type : String,require : true},
+  username: {type : String , require : true},
+  password:{type : String , require : true},
+  email:{type : String , require : true},
+  t_name : {type : String},
+  subject : {type : [String] , require : true},
+  student : {type : mongoose.Types.ObjectId , ref : 'Student'}
+
+});
+
+const Teacher_Modal = mongoose.model('Teacher', Teacher);
+
+export default Teacher_Modal
