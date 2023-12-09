014088019190496ab5c73b0388ac14e5d2e8e80c
ghanashyamkh <khanal.ghanashyam116@gmail.com>
2023-12-09 10:28:10 +0545

    updated submitted assignment modal

--- Model/Submitted_Assignment.js
+++ Model/Submitted_Assignment.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose'
 
 const Submitted_Assignment = new mongoose.Schema({
     assignment : {type : mongoose.Types.ObjectId , ref : "Assignment"},
-    submitted_students : [{type : mongoose.Types.ObjectId,ref : "Student"}]
+    submitted_students_detail : [
+       {student:  {type : mongoose.Types.ObjectId,ref : "Student"}},
+       {submitted_date : Date},
+       {file_path : String}
+    ]
+ 
 })
 
  const Submitted_Assignment_Modal = mongoose.model("Submitted_Assignment" , Submitted_Assignment)
